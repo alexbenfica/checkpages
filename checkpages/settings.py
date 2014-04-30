9922af3423f592f7b4511c2b6a5c7b5f2d462c81
alexbenfica <alexbenfica@gmail.com>
2014-04-30 10:37:54 -0300

    Removed unused pipelines...

--- checkpages/settings.py
+++ checkpages/settings.py
@@ -6,11 +6,11 @@ NEWSPIDER_MODULE = 'checkpages.spiders'
 DEFAULT_ITEM_CLASS = 'checkpages.items.Page'
 
 ITEM_PIPELINES = {'checkpages.pipelines.FilterForbiddenWordsPipeline': 1}
-#ITEM_PIPELINES = {'checkpages.pipelines.FilterWordsPipeline': 1}
 
 
 RETRY_TIMES = 1
 RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
 DOWNLOAD_TIMEOUT = 10
+
 
 LOG_FILE = '/tmp/checkpages-crawler.log'
