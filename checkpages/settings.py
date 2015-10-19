0a54d8b00d50d770d2f3be112e306fe100d44a30
Alex Benfica <alexbenfica@gmail.com>
2015-10-19 12:53:54 -0200

    Modified timeouts to allow faster link checking over night.

--- checkpages/settings.py
+++ checkpages/settings.py
@@ -54,13 +54,13 @@ else:
 # Configurations for page downloads
 RETRY_TIMES = 1
 RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
-DOWNLOAD_TIMEOUT = 10
+DOWNLOAD_TIMEOUT = 8
 
 #Default: 0
 #The amount of time (in secs) that the downloader should wait before downloading 
 #consecutive pages from the same website. 
 #This can be used to throttle the crawling speed to avoid hitting servers too hard.
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 0.2
 
 # Change the order of downloads, getting first those near the root
 DEPTH_PRIORITY = 1
